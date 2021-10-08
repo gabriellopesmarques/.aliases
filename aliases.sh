d5c4df1c56940229d8fd6efa41a1bd4c995e4695
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2021-10-08 10:55:05 -0300

    add alias to check storage, fix version

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 #########################
-##    Aliases v1.1     ##
+##    Aliases v1.3     ##
 #########################
 
 # aliases for repo
@@ -22,6 +22,7 @@ alias static_server='docker run --name "$(basename $(pwd))_static_server" -p 80:
 alias php56_server='docker run --name "$(basename $(pwd))_chialab" -p 80:80 -v "$PWD":/var/www/html/ -d chialab/php:5.6-apache'
 alias php71_server='docker run --name "$(basename $(pwd))_chialab" -p 80:80 -v "$PWD":/var/www/html/ -d chialab/php:7.1-apache'
 alias php72_server='docker run --name "$(basename $(pwd))_chialab" -p 80:80 -v "$PWD":/var/www/html/ -d chialab/php:7.2-apache'
+alias storage='df -h | grep -E -i "\/dev\/sd"'
 
 # dummy ways to write clear
 alias claer=clear
