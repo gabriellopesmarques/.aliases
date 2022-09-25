fa99a33eb57959ef7f430efca9f485d6657ca163
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2022-09-24 23:26:18 -0300

    add i3 install and auto config

--- i3/install.sh
+++ i3/install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# install i3 and utils
 sudo apt-get install xorg i3 i3status feh gnome-terminal pulseaudio brightnessctl
+
+# create i3 config directory
+mkdir -p ~/.config/i3
+
+# link config file
+ln -s ~/.aliases/i3/i3.conf ~/.config/i3/config
+
+# create i3status config directory
+mkdir -p ~/.config/i3status
+
+# link config file
+ln -s ~/.aliases/i3/i3status.conf  ~/.config/i3status/config
+
