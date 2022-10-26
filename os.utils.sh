c4716172ed26bb5a32f3567551a427b86111a673
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2022-10-26 09:18:52 -0300

    add ncdu (ncurses disk usage) and ranger (visual file manager) in utils packages

--- os.utils.sh
+++ os.utils.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get install git ntpdate htop tree bc gnome-screenshot
 
+sudo apt-get install ncdu   # Disk Usage Tool
+sudo apt-get install ranger # console file manager
+
 ## install email server and email reader
 sudo apt-get install postfix mutt -y
 
