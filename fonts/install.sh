883e1b1f565cef9b832b25548c22e8857ce99f13
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-07-26 23:35:25 -0300

    add recursive font

--- fonts/install.sh
+++ fonts/install.sh
@@ -5,4 +5,5 @@ sudo cp -r ./jetbrains_mono/ /usr/share/fonts/truetype/
 sudo cp -r ./Glass_VT220/ /usr/share/fonts/truetype/
 sudo cp -r ./fira/ /usr/share/fonts/truetype/
 sudo cp -r ./ibm_plex/ /usr/share/fonts/truetype/
+sudo cp -r ./recursive/ /usr/share/fonts/truetype/
 
