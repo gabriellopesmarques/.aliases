40980d06fc90b70d8a67ef0d7f97d055e92fd2ca
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-08-20 18:48:23 -0300

    fix path to install fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,9 +1,10 @@
 ## install fonts
-sudo cp -r ./Space_Mono/ /usr/share/fonts/truetype/
-sudo cp -r ./victor_mono/ /usr/share/fonts/truetype/
-sudo cp -r ./jetbrains_mono/ /usr/share/fonts/truetype/
-sudo cp -r ./Glass_VT220/ /usr/share/fonts/truetype/
-sudo cp -r ./fira/ /usr/share/fonts/truetype/
-sudo cp -r ./ibm_plex/ /usr/share/fonts/truetype/
-sudo cp -r ./recursive/ /usr/share/fonts/truetype/
+
+sudo cp -r "$HOME"/.aliases/fonts/Space_Mono/ /usr/share/fonts/truetype/
+sudo cp -r "$HOME"/.aliases/fonts/victor_mono/ /usr/share/fonts/truetype/
+sudo cp -r "$HOME"/.aliases/fonts/jetbrains_mono/ /usr/share/fonts/truetype/
+sudo cp -r "$HOME"/.aliases/fonts/Glass_VT220/ /usr/share/fonts/truetype/
+sudo cp -r "$HOME"/.aliases/fonts/fira/ /usr/share/fonts/truetype/
+sudo cp -r "$HOME"/.aliases/fonts/ibm_plex/ /usr/share/fonts/truetype/
+sudo cp -r "$HOME"/.aliases/fonts/recursive/ /usr/share/fonts/truetype/
 
