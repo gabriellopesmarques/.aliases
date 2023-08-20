692cb4d3944d8cd826ac942c43126726081fb376
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-08-20 18:50:12 -0300

    use double quotes to convention

--- gitui/install.sh
+++ gitui/install.sh
@@ -4,6 +4,6 @@ wget https://github.com/extrawurst/gitui/releases/download/v0.23.0/gitui-linux-m
 tar -xvzf gitui-linux-musl.tar.gz && \
 sudo mv gitui /usr/local/bin/ && \
 rm -rf gitui-linux-*.tar.gz && \
-mkdir -p $HOME/.config/gitui/  && \
-ln -s $HOME/.aliases/gitui/key_bindings.ron $HOME/.config/gitui/
+mkdir -p "$HOME"/.config/gitui/  && \
+ln -s "$HOME"/.aliases/gitui/key_bindings.ron "$HOME"/.config/gitui/
 
