af0e31409b628eba18704ec4485cc5e089048992
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-08-20 12:54:35 -0300

    install gitui by github release

---
+++ gitui/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/extrawurst/gitui/releases/download/v0.23.0/gitui-linux-musl.tar.gz && \
+tar -xvzf gitui-linux-musl.tar.gz && \
+sudo mv gitui /usr/local/bin/ && \
+rm -rf gitui-linux-*.tar.gz && \
+mkdir -p $HOME/.config/gitui/  && \
+ln -s $HOME/.aliases/gitui/key_bindings.ron $HOME/.config/gitui/
+
