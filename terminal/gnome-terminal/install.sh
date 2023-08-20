9767b54c3b3cd41110e30eb83658ca02d13d0e04
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-08-20 11:59:12 -0300

    remove gnome terminal configs

--- terminal/gnome-terminal/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-# install gnome-terminal
-sudo apt-get install gnome-terminal
-
-# import profiles
-dconf load /org/gnome/terminal/legacy/profiles:/ < $HOME/.aliases/terminal/gnome-terminal/gnome-terminal-profiles.dconf
-
