6e1918ea236f3bd240c101f8171a2fcda210a724
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-08-20 12:00:57 -0300

    make vim simple again; remove plugins, remove lsp

--- vim/install.sh
+++ vim/install.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 ## install vim
-sudo apt-get install git curl vim -y
-
-## install plug.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-## install nvm to . . .
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-## reload vars and install node lts
-source ~/.zshrc && nvm install --lts
+sudo apt install git curl vim -y
 
 ## load .aliases vim configs
 echo "source ~/.aliases/vim/vimrc" >> ~/.vimrc
-
-## run plug install quiet
-vim +PlugInstall +qall
 
