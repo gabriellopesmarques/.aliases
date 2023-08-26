02bddb54ab147d053e9b64b26cf1d7992547707f
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2023-08-26 14:51:31 -0300

    fix gitui installer script

--- gitui/install.sh
+++ gitui/install.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-wget https://github.com/extrawurst/gitui/releases/download/v0.23.0/gitui-linux-musl.tar.gz && \
-tar -xvzf gitui-linux-musl.tar.gz && \
-sudo mv gitui /usr/local/bin/ && \
-rm -rf gitui-linux-*.tar.gz && \
+dir="$HOME/.aliases/gitui"
+
+wget -P "$dir" https://github.com/extrawurst/gitui/releases/download/v0.23.0/gitui-linux-musl.tar.gz && \
+tar -xzf "$dir"/gitui-linux-musl.tar.gz -C "$dir" && \
+sudo mv "$dir"/gitui /usr/local/bin/ && \
+rm -rf "$dir"/gitui-linux-*.tar.gz && \
 mkdir -p "$HOME"/.config/gitui/  && \
 ln -s "$HOME"/.aliases/gitui/key_bindings.ron "$HOME"/.config/gitui/
 
