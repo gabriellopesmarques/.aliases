7a7ec3da7ab18fe31c7dabd2df7b912f40fbb92e
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2024-04-26 13:03:03 -0300

    install brightnessctl if is a laptop

--- install.sh
+++ install.sh
@@ -10,11 +10,10 @@ install_aliases () {
 install_utils () {
     echo "installing utils tools"
     sudo apt install ncdu shellcheck exa fzf jq -y
-    #brightnessctl
 
     # if is a laptop install network manager
     if [[ -d "/proc/acpi/button/lid" ]]; then
-        sudo apt install network-manager -y
+        sudo apt install network-manager brightnessctl -y
     fi
 
     bash "$HOME"/.aliases/bat/install.sh     && \
