2772cc0bade43b6947c6a8b7b748801d4fb74bb7
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2025-04-12 16:56:55 -0300

    refactor(gitui): always get latest version; update keybindings to use vim motion in newer version

--- gitui/install.sh
+++ gitui/install.sh
@@ -2,10 +2,24 @@
 
 dir="$HOME/.aliases/gitui"
 
-wget -P "$dir" https://github.com/extrawurst/gitui/releases/download/v0.23.0/gitui-linux-musl.tar.gz && \
-tar -xzf "$dir"/gitui-linux-musl.tar.gz -C "$dir" && \
+for cmd in wget jq; do
+    if ! command -v "$cmd" >/dev/null 2>&1; then
+        echo "[! error !] required package ${cmd} is not installed"
+        exit 1
+    fi
+done
+
+last_release=$(wget -q -O - https://api.github.com/repos/gitui-org/gitui/releases/latest | jq -r '.assets | .[] | select(.name == "gitui-linux-x86_64.tar.gz") | .browser_download_url')
+
+if [[ ! "$last_release" == http* ]]; then
+    echo "[! error !] cant get gitui latest version from github api"
+    exit 1
+fi
+
+wget -P "$dir" "$last_release" && \
+tar -xzf "$dir"/*.tar.gz -C "$dir" && \
 sudo mv "$dir"/gitui /usr/local/bin/ && \
-rm -rf "$dir"/gitui-linux-*.tar.gz && \
+rm -rf "$dir"/*.tar.gz && \
 mkdir -p "$HOME"/.config/gitui/  && \
 ln -s "$HOME"/.aliases/gitui/key_bindings.ron "$HOME"/.config/gitui/
 
