c92a929c228ac084daec7133b123d1fc5651b318
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2025-04-13 10:15:43 -0300

    refactor(gitui): install requirements by default; use curl instead wget

--- gitui/install.sh
+++ gitui/install.sh
@@ -2,21 +2,16 @@
 
 dir="$HOME/.aliases/gitui"
 
-for cmd in wget jq; do
-    if ! command -v "$cmd" >/dev/null 2>&1; then
-        echo "[! error !] required package ${cmd} is not installed"
-        exit 1
-    fi
-done
+sudo apt install curl jq -y
 
-last_release=$(wget -q -O - https://api.github.com/repos/gitui-org/gitui/releases/latest | jq -r '.assets | .[] | select(.name == "gitui-linux-x86_64.tar.gz") | .browser_download_url')
+last_release=$(curl --silent https://api.github.com/repos/gitui-org/gitui/releases/latest | jq -r '.assets | .[] | select(.name == "gitui-linux-x86_64.tar.gz") | .browser_download_url')
 
 if [[ ! "$last_release" == http* ]]; then
     echo "[! error !] cant get gitui latest version from github api"
     exit 1
 fi
 
-wget -P "$dir" "$last_release" && \
+curl -L -o "${dir}/gitui.tar.gz" "$last_release" && \
 tar -xzf "$dir"/*.tar.gz -C "$dir" && \
 sudo mv "$dir"/gitui /usr/local/bin/ && \
 rm -rf "$dir"/*.tar.gz && \
