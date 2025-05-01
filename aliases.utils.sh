832929733a20b66f5c9a4e039ed0256927df0c47
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2025-05-01 15:06:52 -0300

    feat(utils): improve man pager (less) settings

--- aliases.utils.sh
+++ aliases.utils.sh
@@ -7,6 +7,19 @@ alias glow="glow -s ~/.config/glow/nord.json"
 alias fd='cd $(find -type d 2>/dev/null | fzf); pwd'
 alias calendar="dialog --calendar 'calendar' 0 0"
 
+# improve less settings
+if [[ -n "$PS1" ]] && [[ "$TERM" != "dumb" ]]; then
+  export MANPAGER="less -R"
+  export GROFF_NO_SGR=0
+  export LESS_TERMCAP_mb=$'\E[01;31m'
+  export LESS_TERMCAP_md=$'\E[01;31m'
+  export LESS_TERMCAP_me=$'\E[0m'
+  export LESS_TERMCAP_se=$'\E[0m'
+  export LESS_TERMCAP_so=$'\E[01;44;33m'
+  export LESS_TERMCAP_ue=$'\E[0m'
+  export LESS_TERMCAP_us=$'\E[01;32m'
+fi
+
 ai(){
     if [[ -z "$GROQ_API_KEY" ]]; then
         echo "declare GROQ_API_KEY to use this function"
@@ -21,3 +34,4 @@ ai(){
          -H "Content-Type: application/json" \
          -d '{"messages": [{"role": "user", "content": "'"$question"'"}], "model": "llama3-8b-8192"}' | jq -r '.choices[0].message.content' | glow -
 }
+
