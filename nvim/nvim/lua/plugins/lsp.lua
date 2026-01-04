0194cc145f4a9f5e93c07127dc3466627b5db2fb
Gabriel Lopes <gabriellopesmarques@hotmail.com>
2026-01-04 14:26:08 -0300

    chore: update lsp configs to nvim v0.11+

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -1,87 +1,88 @@
 local lsp = require('lsp-zero')
 local cmp = require('cmp')
+
 local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
 
 cmp.setup({
-        sources = {
-            {name = 'nvim_lsp'},
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select_opts),
+        ['<Tab>'] = cmp.mapping.select_next_item(cmp_select_opts),
+        ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_prev_item(cmp_select_opts)
+            else
+                cmp.complete()
+            end
+        end),
+        ['<C-n>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_next_item(cmp_select_opts)
+            else
+                cmp.complete()
+            end
+        end),
+    },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    window = {
+        documentation = {
+            max_height = 15,
+            max_width = 60,
         },
-        mapping = {
-            ['<CR>'] = cmp.mapping.confirm({select = true}),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-d>'] = cmp.mapping.scroll_docs(4),
-            ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select_opts),
-            ['<Tab>'] = cmp.mapping.select_next_item(cmp_select_opts),
-            ['<C-p>'] = cmp.mapping(function()
-                if cmp.visible() then
-                    cmp.select_prev_item(cmp_select_opts)
-                else
-                    cmp.complete()
-                end
-            end),
-            ['<C-n>'] = cmp.mapping(function()
-                if cmp.visible() then
-                    cmp.select_next_item(cmp_select_opts)
-                else
-                    cmp.complete()
-                end
-            end),
-        },
-        snippet = {
-            expand = function(args)
-                require('luasnip').lsp_expand(args.body)
-            end,
-        },
-        window = {
-            documentation = {
-                max_height = 15,
-                max_width = 60,
+    },
+    formatting = {
+        fields = { 'abbr', 'menu', 'kind' },
+        format = function(entry, item)
+            local short_name = {
+                nvim_lsp = 'LSP',
+                nvim_lua = 'nvim',
             }
-        },
-        formatting = {
-            fields = {'abbr', 'menu', 'kind'},
-            format = function(entry, item)
-                local short_name = {
-                    nvim_lsp = 'LSP',
-                    nvim_lua = 'nvim'
-                }
+            item.menu = string.format('[%s]', short_name[entry.source.name] or entry.source.name)
+            return item
+        end,
+    },
+})
 
-                local menu_name = short_name[entry.source.name] or entry.source.name
-
-                item.menu = string.format('[%s]', menu_name)
-                return item
-            end,
-        },
-    })
-
-lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({buffer = bufnr})
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
+lsp.on_attach(function(_, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
 end)
 
+vim.lsp.config('lua_ls', lsp.nvim_lua_ls())
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
+--------------------------------------------------
+-- Mason
+--------------------------------------------------
 require('mason').setup()
+
 require('mason-lspconfig').setup({
-        ensure_installed = {
-            'clangd',
-            'bashls',
-            'lua_ls',
-            'intelephense',
-            'pyright',
-            'dockerls',
-            'docker_compose_language_service',
-        },
+    ensure_installed = {
+        'clangd',
+        'bashls',
+        'lua_ls',
+        'intelephense',
+        'pyright',
+        'dockerls',
+        'docker_compose_language_service',
+    },
 
-        handlers = {
-            function(server_name)
-                require('lspconfig')[server_name].setup({})
-            end,
-        },
+    handlers = {
+        function(server_name)
+            -- novo método (sem require("lspconfig"))
+            vim.lsp.config(server_name, {})
+        end,
+    },
+})
 
-    })
 lsp.setup()
 
